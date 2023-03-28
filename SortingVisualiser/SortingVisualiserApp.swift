ca342cb5dad22684a08c7cad99d5d0098562eacc
Taher Master <master.taherm@gmail.com>
2023-03-28 18:47:02 -0400

    Initial Commit

---
+++ SortingVisualiser/SortingVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingVisualiserApp.swift
+//  SortingVisualiser
+//
+//  Created by Taher Master on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct SortingVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
